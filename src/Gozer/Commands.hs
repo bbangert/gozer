7b6544409390b1a5f0795f121b9d0d2d0af00aaa
Ben Bangert <ben@groovie.org>
2014-04-24 11:15:46 -0700

    Switch to using monad transformers.

--- src/Gozer/Commands.hs
+++ src/Gozer/Commands.hs
@@ -1,42 +1,64 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Gozer.Commands (
-    loadTweets,
-    tweetsNewerThan,
-    deleteTweets,
     deleteOlder,
     ) where
 
-import           Control.Applicative  ((<$>))
-import           Control.Monad        (unless)
-import           Crypto.Random        (SystemRNG, cprgFork)
-import           Data.Aeson           (decode, (.:))
-import           Data.Aeson.Types     (parseMaybe)
-import           Data.ByteString.Lazy (ByteString)
-import           Data.Maybe           (fromJust)
-import           Data.Time.Clock      (NominalDiffTime, UTCTime, addUTCTime,
-                                       getCurrentTime)
-import           Network.HTTP.Client  (Manager, Request, Response, httpLbs,
-                                       method, parseUrl, responseBody)
-import           Network.OAuth        (Cred, Permanent, defaultServer, oauth)
-import           Pipes                (Consumer, Producer, await, lift,
-                                       runEffect, yield, (>->))
-import qualified Pipes.Prelude        as PL
+import           Control.Applicative        ((<$>))
+import           Control.Monad              (unless, void)
+import           Control.Monad.Reader       (ReaderT, runReaderT, ask)
+import           Control.Monad.State.Strict (StateT, runStateT, get, put)
+import           Control.Monad.Trans        (liftIO)
+import           Crypto.Random              (SystemRNG, cprgFork)
+import           Data.Aeson                 (decode, (.:))
+import           Data.Aeson.Types           (parseMaybe)
+import           Data.ByteString.Lazy       (ByteString)
+import           Data.Maybe                 (fromJust)
+import           Data.Time.Clock            (NominalDiffTime, UTCTime,
+                                             addUTCTime, getCurrentTime)
+import           Network.HTTP.Client        (Manager, Request, Response,
+                                             httpLbs, method, parseUrl,
+                                             responseBody)
+import           Network.OAuth              (Cred, Permanent, defaultServer,
+                                             oauth)
+import           Pipes                      (Consumer, Producer, await, lift,
+                                             runEffect, yield, (>->))
+import qualified Pipes.Prelude              as PL
 
-import           Gozer.Config         (ConfigSettings (ConfigSettings))
-import           Gozer.Types          (Tweet, tweetCreatedAt, tweetId)
+import           Gozer.Config               (ConfigSettings (ConfigSettings))
+import           Gozer.Types                (Tweet, tweetCreatedAt, tweetId)
+
+data TwitterConfig = TwitterConfig {
+    tweetCreds   :: Cred Permanent
+  , tweetManager :: Manager
+}
+
+type TwitterCommand = ReaderT TwitterConfig (StateT SystemRNG IO)
+
+fromUrl :: String -> Request
+fromUrl = fromJust . parseUrl
+
+-- | Decode the tweet count from a JSON response
+decodeTweetCount :: ByteString -> Int
+decodeTweetCount = fromJust . parseMaybe (.: "statuses_count") . fromJust . decode
+
+runTwitterCommand :: TwitterCommand a -> TwitterConfig -> SystemRNG
+                  -> IO (a, SystemRNG)
+runTwitterCommand tc conf rng = runStateT (runReaderT tc conf) rng
 
 -- | Signs and runs a request to get back a response
-runRequest :: Request -> Cred Permanent -> Manager -> SystemRNG
-              -> IO (Response ByteString,  SystemRNG)
-runRequest req creds m gen = do
-  (signedReq, gen') <- oauth creds defaultServer req gen
-  resp <- httpLbs signedReq m
-  return (resp, gen')
+runRequest :: Request -> TwitterCommand (Response ByteString)
+runRequest req = do
+  TwitterConfig creds m <- ask
+  gen <- get
+  (signedReq, gen') <- liftIO $ oauth creds defaultServer req gen
+  put gen'
+  resp <- liftIO $ httpLbs signedReq m
+  return resp
 
 -- | Timeline URL for retrieving tweets
 timelineUrl :: String -> Maybe Integer -> Request
-timelineUrl name sinceId = fromJust . parseUrl $ concat
+timelineUrl name sinceId = fromUrl $ concat
   [ "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name="
   , name
   , "&include_rts=true&count=200"
@@ -46,41 +68,46 @@ timelineUrl name sinceId = fromJust . parseUrl $ concat
 -- | Destroy URL for deleting tweets
 destroyUrl :: Integer -> Request
 destroyUrl i = (makeUrl i) {method="POST"}
-  where makeUrl = fromJust . parseUrl
+  where makeUrl = fromUrl
           . ("https://api.twitter.com/1.1/statuses/destroy/"++)
           . (++".json") . show
 
 -- | Tweet information for the user
 informationURL :: String -> Request
-informationURL name = fromJust . parseUrl $
+informationURL name = fromUrl $
   "https://api.twitter.com/1.1/users/show.json?screen_name=" ++ name
-
--- | Decode the tweet count from a JSON response
-decodeTweetCount :: ByteString -> Int
-decodeTweetCount = fromJust . parseMaybe (.: "statuses_count") . fromJust . decode
 
 -- | Return the difference between the users current tweets and the given
 -- vale. If Nothing, the full count is returned, 0 is returned if its negative.
-tweetDiff :: String -> Maybe Int -> Cred Permanent -> Manager -> SystemRNG -> IO Int
-tweetDiff name tweetMin creds m gen = do
-  resp <- fst <$> runRequest (informationURL name) creds m gen
+tweetDiff :: String -> Maybe Int -> TwitterCommand Int
+tweetDiff name tweetMin = do
+  resp <- runRequest (informationURL name)
   let tweetCount = decodeTweetCount $ responseBody resp
   return $ maybe tweetCount (\i -> max (tweetCount-i) 0) tweetMin
+
+-- | Loads tweets starting at the offset asked
+loadTweets :: String -> Maybe Integer -> TwitterCommand [Tweet]
+loadTweets name index = do
+  resp <- runRequest $ timelineUrl name index
+  return $ fromJust (decode (responseBody resp) :: Maybe [Tweet])
+
+-- | Delete a tweet
+deleteTweet :: Tweet -> TwitterCommand ()
+deleteTweet = void . runRequest . destroyUrl . tweetId
 
 -- | Produces tweets as long as asked to by loading more tweets from twitter
 -- Stops producing tweets if the last result is the same as the index we
 -- start at
-loadTweets :: String -> Maybe Integer -> Cred Permanent -> Manager -> SystemRNG
+tweetProducer :: String -> Maybe Integer -> TwitterConfig -> SystemRNG
               -> Producer Tweet IO ()
-loadTweets name index creds m gen = do
-  (resp, gen') <- lift $ runRequest (timelineUrl name index) creds m gen
-  let tweets = fromJust (decode (responseBody resp) :: Maybe [Tweet])
+tweetProducer name index tc gen = do
+  (tweets, gen') <- lift $ runTwitterCommand (loadTweets name index) tc gen
   mapM_ yield tweets
   unless (null tweets) $ do
     let lastId = tweetId $ last tweets
     case index of
-      Just x -> unless (x == lastId) $ loadTweets name (Just lastId) creds m gen'
-      Nothing -> loadTweets name (Just lastId) creds m gen'
+      Just x -> unless (x == lastId) $ tweetProducer name (Just lastId) tc gen'
+      Nothing -> tweetProducer name (Just lastId) tc gen'
 
 -- | Indicates if the tweet is newer than the time diff + a time
 tweetsNewerThan :: NominalDiffTime -> UTCTime -> Tweet -> Bool
@@ -88,21 +115,21 @@ tweetsNewerThan period now t = tweetCreatedAt t > oldTime
   where oldTime = addUTCTime period now
 
 -- | Issues a delete API call for every tweet received
-deleteTweets :: Cred Permanent -> Manager -> SystemRNG
-                -> Consumer Tweet IO ()
-deleteTweets creds m gen = do
+tweetDeleter :: TwitterConfig -> SystemRNG -> Consumer Tweet IO ()
+tweetDeleter tc gen = do
   tweet <- await
-  (_, gen') <- lift $ runRequest (destroyUrl $ tweetId tweet) creds m gen
-  deleteTweets creds m gen'
+  (_, gen') <- lift $ runTwitterCommand (deleteTweet tweet) tc gen
+  tweetDeleter tc gen'
 
 -- Wraps up several components to run the complete delete pipeline
 deleteOlder :: Manager -> SystemRNG -> ConfigSettings -> IO ()
 deleteOlder m gen (ConfigSettings name days creds tweetMin) = do
   let (diffGen, gen') = cprgFork gen
       (loadGen, deleteGen) = cprgFork gen'
-  tweetNum <- tweetDiff name tweetMin creds m diffGen
+      tc = TwitterConfig creds m
+  (tweetNum, _) <- runTwitterCommand (tweetDiff name tweetMin) tc diffGen
   newer <- tweetsNewerThan (-60*60*24*days) <$> getCurrentTime
-  runEffect $ loadTweets name Nothing creds m loadGen
+  runEffect $ tweetProducer name Nothing tc loadGen
           >-> PL.dropWhile newer
           >-> PL.take tweetNum
-          >-> deleteTweets creds m deleteGen
+          >-> tweetDeleter tc deleteGen
