a5312bbab49e16270d834b4633696da0c34e158e
Ben Bangert <ben@groovie.org>
2014-05-24 15:04:53 -0700

    minor tweaks

--- src/Gozer/Config.hs
+++ src/Gozer/Config.hs
@@ -6,8 +6,7 @@ module Gozer.Config (
 import           Control.Applicative   ((<$>), (<*>))
 import           Control.Monad.Error   (join, liftIO, runErrorT)
 import           Data.ByteString.Char8 (pack)
-import           Data.ConfigFile       (CPError, emptyCP, get,
-                                        readfile)
+import           Data.ConfigFile       (CPError, emptyCP, get, readfile)
 import           Data.Time.Clock       (NominalDiffTime)
 import           Network.OAuth         (Cred, Permanent, Token (..), clientCred,
                                         permanentCred)
@@ -28,12 +27,13 @@ parseConfigFile filename = runErrorT $ do
                        <*> ecp "access_token_secret"
   clientToken <- Token <$> ecp "api_key"
                        <*> ecp "api_secret"
-  time <- get cp "DEFAULT" "duration"
-  username <- get cp "DEFAULT" "username"
+  time        <- dget cp "duration"
+  username    <- dget cp "username"
   maintainNum <- either (const Nothing) readMaybe <$>
-    runErrorT (get cp "DEFAULT" "minimum_tweets")
+    runErrorT (dget cp "minimum_tweets")
   let dur = fromIntegral (time :: Int)
       creds = permanentCred accessToken $ clientCred clientToken
   return $ ConfigSettings username dur creds maintainNum
   where
-    extractPack cp name = pack <$> get cp "DEFAULT" name
+    dget cp = get cp "DEFAULT"
+    extractPack cp name = pack <$> dget cp name
